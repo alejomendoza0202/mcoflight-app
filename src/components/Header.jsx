404aa1a77047bde4e3360b903f2b4b0a66fb740b
Alejandro <alejomendoza0202@gmail.com>
2022-04-01 19:56:48 -0500

    bug fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./Header.module.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const Header = () => {
   return (
